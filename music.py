33cab578223df9968ab8e441fd9af552a6f786e9
Ian Thompson <ianthompson521@gmail.com>
2020-07-29 23:07:39 -0400

    Help message

--- music.py
+++ music.py
@@ -20,8 +20,16 @@ def cli():
         # List options
         if command == 'help':
             print('helping')
+            print('Enter albums to view all albums')
+            print('Enter songs to view all songs')
+            print('Enter artists to view all artists')
+            print('Enter queue with file or dir name to send')
 
-        os.system(command)
-        exit()
+        elif command == 'exit':
+            print('Shutting down...')
+            exit()
+
+        else:
+            os.system(command)
 
 cli()
