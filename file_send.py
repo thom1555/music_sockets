929917f3c3de9673e8b33e03699005ef0fb90f6f
Ian Thompson <ianthompson521@gmail.com>
2020-07-30 19:14:16 -0400

    Clean up code

--- file_send.py
+++ file_send.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python3
+"""
+Send a file to a receiving socket
+"""
 import socket
-import tqdm
 import os
+import tqdm
 
 
 def send_file(ip_addr, port, filename):
@@ -8,37 +12,37 @@ def send_file(ip_addr, port, filename):
     Open socket and send file
     """
     # Define constants
-    FILE_SIZE = os.path.getsize(filename)
-    SEPARATOR = "<SEPARATOR>"
-    BUFFER_SIZE = 4096 # send 4096 bytes each time step
+    file_size = os.path.getsize(filename)
+    separator = "<separator>"
+    buffer_size = 4096 # send 4096 bytes each time step
 
     # Open socket
-    s = socket.socket()
+    sending_socket = socket.socket()
     print(f"[+] Connecting to {ip_addr}:{port}")
-    s.connect((ip_addr, port))
+    sending_socket.connect((ip_addr, port))
     print("[+] Connected.")
 
     # Send filename and size
-    s.send(f"{filename}{SEPARATOR}{FILE_SIZE}".encode())
+    sending_socket.send(f"{filename}{separator}{file_size}".encode())
 
     # Start sending file
-    progress = tqdm.tqdm(range(FILE_SIZE), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    progress = tqdm.tqdm(range(file_size), f"Sending {filename}", \
+            unit="B", unit_scale=True, unit_divisor=1024)
 
-    with open(filename, "rb") as f:
+    with open(filename, "rb") as input_file:
         for _ in progress:
             # Read bytes from file
-            bytes_read = f.read(BUFFER_SIZE)
+            bytes_read = input_file.read(buffer_size)
             if not bytes_read:
                 # Transmitting is done
                 break
             # Sendall to assure transmission
             # Might change with wired
-            s.sendall(bytes_read)
+            sending_socket.sendall(bytes_read)
             # Update progress bar
             progress.update(len(bytes_read))
 
     # close socket
-    s.close()
+    sending_socket.close()
 
 #send_file("192.168.50.161", 5001, "./example_files/UMVPN-4.8.03052.tar.gz")
-
