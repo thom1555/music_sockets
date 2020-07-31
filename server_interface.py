bdc0b8f60349414be0401fd12b353228bab3daf4
Ian Thompson <ianthompson521@gmail.com>
2020-07-30 20:19:32 -0400

    Add change port function

--- server_interface.py
+++ server_interface.py
@@ -4,6 +4,7 @@ Command Line Interface for music distribution
 
 from os import listdir, system
 from os.path import isfile, join
+import socket
 import sys
 import file_send
 
@@ -17,7 +18,7 @@ class Music:
         List of ips in cluster
         Port to communicate over
         """
-        self.ip_addr = ["192.168.50.161"]
+        self.ip_addr = ["192.168.50.66"]
         self.port = 5001
 
 
@@ -33,7 +34,13 @@ class Music:
         """
         Send message to all client to change port to communicate over
         """
-        pass
+        for address in self.ip_addr:
+            sending_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sending_socket.connect((address, self.port))
+            sending_socket.msg('Test Message')
+            sending_socket.close()
+
+        return True
 
 
     def file_loop(self):
@@ -87,8 +94,11 @@ class Music:
                 print('Enter artists to view all artists')
                 print('Enter queue with file or dir name to send')
 
-            elif command == 'sendfile':
+            elif command == 'send file':
                 self.file_loop()
+
+            elif command == 'change port':
+                self.change_port(5002)
 
             elif command == 'exit':
                 print('Shutting down...')
